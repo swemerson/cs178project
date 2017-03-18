879702c7dd8df5c9c3a784746fe37116e5766fd6
Sean <swemerson@gmail.com>
2017-03-18 15:03:20 -0700

    Added AUC function for testing

--- Blend.py
+++ Blend.py
@@ -3,7 +3,7 @@ import mltools as ml
 import matplotlib.pyplot as plt
 import Dtree
 import Knn
-import Linear
+import linear
 
 from sklearn.metrics import mean_squared_error
 
@@ -20,22 +20,22 @@ if (generateKaggleFile):
     sampleSize = 100000  # Don't change this
     split = 1  # Don't change this
 else:
-    sampleSize = 50000
+    sampleSize = 5000
     split = 0.75
 
 # Using
 usingDtree = 1
 usingKnn = 1
-usingLinear = 0
+usingLinear = 1
 
 # Dtree Settings
-maxDepth = 6
-minLeaf = 128
-nFeatures = 5
-nTrees = 100
+maxDepth = 19
+minLeaf = 64
+nFeatures = 12
+nTrees = 200
 
 # Knn Settings
-n_neighbors = 15
+n_neighbors = 39
 
 # Linear Settings
 deg = 1
@@ -70,7 +70,7 @@ def predictSoft(Xte):
         Yhat = np.column_stack((Yhat, Knn.predictSoft(Xtr, Ytr, Xte, n_neighbors)))
 
     if (usingLinear):
-        Yhat = np.column_stack((Yhat, Linear.predictSoft(Xtr, Ytr, Xte, deg)))
+        Yhat = np.column_stack((Yhat, linear.predictSoft(Xtr, Ytr, Xte, deg)))
 
     return np.mean(Yhat[:, 1:], axis=1)
 
@@ -78,8 +78,29 @@ def predictSoft(Xte):
 # Calculate MSE
 def mse():
     mean = mean_squared_error(predictSoft(Xva), Yva)
-    print 'Mean squared error: %.10f' % mean
+    print 'MSE: ' + str(mean)
     return mean
+
+# Calculate AUC
+def auc(soft, Y):
+    """Manual AUC function for applying to soft prediction vectors"""
+    indices = np.argsort(soft)  # sort data by score value
+    Y = Y[indices]
+    sorted_soft = soft[indices]
+
+    # compute rank (averaged for ties) of sorted data
+    dif = np.hstack(([True], np.diff(sorted_soft) != 0, [True]))
+    r1 = np.argwhere(dif).flatten()
+    r2 = r1[0:-1] + 0.5 * (r1[1:] - r1[0:-1]) + 0.5
+    rnk = r2[np.cumsum(dif[:-1]) - 1]
+
+    # number of true negatives and positives
+    n0, n1 = sum(Y == 0), sum(Y == 1)
+
+    # compute AUC using Mann-Whitney U statistic
+    result = (np.sum(rnk[Y == 1]) - n1 * (n1 + 1.0) / 2.0) / n1 / n0
+    print 'AUC: ' + str(result)
+    return result
 
 # Generate file for Kaggle submission
 def toKaggle():
@@ -96,13 +117,13 @@ def toKaggle():
 if generateKaggleFile:
     toKaggle()
 else:
-    global n_neighbors
-    means = [0] * 25
-    for i in range(30, 55):
-        print i
-        n_neighbors = i
-        means[i-30] = mse()
+    #global nFeatures
+    #aucs = [0] * 10
+    #for i in range(5, 16):
+    #    print i
+    #    nFeatures = i
+    #    aucs[i-5] = auc(predictSoft(Xva), Yva)
 
-    plt.plot(means)
-    plt.show()
-
+    #plt.plot(aucs)
+    #plt.show()
+    auc(predictSoft(Xva), Yva)
